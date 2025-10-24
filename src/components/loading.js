1034710d518d44dc11890b5e14ce2e2e8367b0f9
Doris Chen <43398278+dsmchen@users.noreply.github.com>
2025-10-24 12:09:20 +0800

    Remove unnecessary code

--- src/components/loading.js
+++ src/components/loading.js
@@ -1,13 +1,9 @@
 export function showLoadingSpinner() {
   const overlay = document.querySelector(".overlay");
   overlay.style.display = "flex";
-  overlay.setAttribute("aria-hidden", "false");
-  document.body.style.overflow = "hidden"; // Disable scrolling when overlay is displayed
 }
 
 export function hideLoadingSpinner() {
   const overlay = document.querySelector(".overlay");
   overlay.style.display = "none";
-  overlay.setAttribute("aria-hidden", "true");
-  document.body.style.overflow = "auto"; // Enable scrolling after hiding overlay
 }
